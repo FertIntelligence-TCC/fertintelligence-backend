930aeea4d9dcef36118d6aff827bd10b9cadc875
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2025-07-15 09:31:15 -0300

    Fix: application.properties, dependencies, UserController documentation & implementation.

---
+++ src/main/java/com/migueltcc/fertintelligence/controller/documentation/UserController.java
@@ -0,0 +1,28 @@
+package com.migueltcc.fertintelligence.controller.documentation;
+
+import com.migueltcc.fertintelligence.dto.user.UserCreateRequestDto;
+import com.migueltcc.fertintelligence.dto.user.UserPostRequestDto;
+import com.migueltcc.fertintelligence.dto.user.UserResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Usuário", description = "Serviço de Usuário")
+public interface UserController {
+    @Operation(description = "Registrar usuário", summary = "Registrar novo usuário no sistema")
+    ResponseEntity<String> createUser(
+            @RequestBody UserCreateRequestDto userDto);
+
+    @Operation(description = "Atualizar usuário", summary = "Atualizar usuário no sistema. Deixe o campo vazio para mantê-lo.")
+    ResponseEntity<String> updateUser(
+            @RequestBody() UserPostRequestDto userDto,
+            Authentication authentication);
+
+    @Operation(description = "Deletar usuário", summary = "Remover usuário do sistema.")
+    ResponseEntity<String> deleteUser(Authentication authentication);
+
+    @Operation(description = "Ler usuário", summary = "Pegar informações do usuário no sistema.")
+    ResponseEntity<UserResponseDto> getUser(Authentication authentication);
+}
---
+++ src/main/java/com/migueltcc/fertintelligence/controller/implementation/UserControllerImpl.java
@@ -0,0 +1,51 @@
+package com.migueltcc.fertintelligence.controller.implementation;
+
+import com.migueltcc.fertintelligence.controller.documentation.UserController;
+import com.migueltcc.fertintelligence.dto.user.UserCreateRequestDto;
+import com.migueltcc.fertintelligence.dto.user.UserPostRequestDto;
+import com.migueltcc.fertintelligence.dto.user.UserResponseDto;
+import com.migueltcc.fertintelligence.service.documentation.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+@CrossOrigin
+@Slf4j
+public class UserControllerImpl implements UserController {
+    private final UserService userService;
+    @Autowired
+    public UserControllerImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@Validated @RequestBody UserCreateRequestDto userDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userDto));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateUser(
+            @Validated @RequestBody UserPostRequestDto userDto,
+            Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.postUser(authentication.getName(), userDto));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteUser(
+            Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.deleteUser(authentication.getName()));
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<UserResponseDto> getUser(
+            Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(authentication.getName()));
+    }
+}
