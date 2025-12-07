5c86e73007b8c742433f9927087b852f7fe3eadd
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2025-12-07 16:58:41 -0300

    Feats: Model e DTOs de OrganoMineralFertilizerModel.

---
+++ src/main/java/com/migueltcc/fertintelligence/model/fertintelligence/soilFertilizerModels/OrganoMineralFertilizerModel.java
@@ -0,0 +1,101 @@
+package com.migueltcc.fertintelligence.model.fertintelligence.soilFertilizerModels;
+
+import com.migueltcc.fertintelligence.dto.fertilizers.soilFertilizers.organoMineralFertilizer.OrganoMineralFertilizerResponseDto;
+import com.migueltcc.fertintelligence.model.fertintelligence.UserModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Data
+@Table(name = "ADUBOS_ORGANO_MINERAIS")
+@EqualsAndHashCode
+public class OrganoMineralFertilizerModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_CRIADOR", nullable = false)
+    private UserModel user;
+
+    @Column(name = "NOME_ADUBO")
+    private String name;
+
+    // Componente Orgânico
+    @Column(name = "PORCENTAGEM_CARBONO")
+    private Double C;
+
+    // Macronutrientes Primários
+    @Column(name = "PORCENTAGEM_NITROGENIO")
+    private Double N;
+
+    @Column(name = "PORCENTAGEM_P2O5")
+    private Double P2O5;
+
+    @Column(name = "PORCENTAGEM_K2O")
+    private Double K2O;
+
+    // Macronutrientes Secundários
+    @Column(name = "PORCENTAGEM_CALCIO")
+    private Double Ca;
+
+    @Column(name = "PORCENTAGEM_MAGNESIO")
+    private Double Mg;
+
+    @Column(name = "PORCENTAGEM_ENXOFRE")
+    private Double S;
+
+    // Micronutrientes
+    @Column(name = "PORCENTAGEM_BORO")
+    private Double B;
+
+    @Column(name = "PORCENTAGEM_COBRE")
+    private Double Cu;
+
+    @Column(name = "PORCENTAGEM_FERRO")
+    private Double Fe;
+
+    @Column(name = "PORCENTAGEM_MANGANES")
+    private Double Mn;
+
+    @Column(name = "PORCENTAGEM_MOLIBDENIO")
+    private Double Mo;
+
+    @Column(name = "PORCENTAGEM_ZINCO")
+    private Double Zn;
+
+    // Índices Físico-Químicos
+    @Column(name = "INDICE_SALINO")
+    private Double indiceSalino;
+
+    @Column(name = "INDICE_ACIDEZ")
+    private Double indiceAcidez;
+
+    public OrganoMineralFertilizerResponseDto toDto() {
+        return OrganoMineralFertilizerResponseDto.builder()
+                .id(this.id)
+                .name(this.name)
+                .c(this.C)
+                .n(this.N)
+                .p2o5(this.P2O5)
+                .k2o(this.K2O)
+                .ca(this.Ca)
+                .mg(this.Mg)
+                .s(this.S)
+                .b(this.B)
+                .cu(this.Cu)
+                .fe(this.Fe)
+                .mn(this.Mn)
+                .mo(this.Mo)
+                .zn(this.Zn)
+                .indiceSalino(this.indiceSalino)
+                .indiceAcidez(this.indiceAcidez)
+                .userId(this.user != null ? this.user.getId() : null)
+                .userNome(this.user != null ? this.user.getName() : null)
+                .build();
+    }
+}
