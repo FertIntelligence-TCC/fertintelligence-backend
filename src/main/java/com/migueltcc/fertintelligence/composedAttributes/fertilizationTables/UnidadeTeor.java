01a907d36681d2978c3ee27f8b1b293d75724ed1
Miguel Macedo Ferreira <132075645+Mikeru-UFCG@users.noreply.github.com>
2026-05-25 22:48:35 -0300

    Refatora critérios de calagem e amplia UnidadeTeor

--- src/main/java/com/migueltcc/fertintelligence/composedAttributes/fertilizationTables/UnidadeTeor.java
+++ src/main/java/com/migueltcc/fertintelligence/composedAttributes/fertilizationTables/UnidadeTeor.java
@@ -3,4 +3,10 @@ package com.migueltcc.fertintelligence.composedAttributes.fertilizationTables;
 public enum UnidadeTeor {
     dag_per_kg,
     mg_per_kg,
+    g_per_kg,
+    g_per_dm3,
+    mg_per_dm3,
+    cmolc_per_dm3,
+    mmolc_per_dm3,
+    percentage,
 }
