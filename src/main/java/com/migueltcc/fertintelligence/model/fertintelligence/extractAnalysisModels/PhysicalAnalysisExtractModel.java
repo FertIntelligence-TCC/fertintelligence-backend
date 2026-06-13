f7034a8857eae05229009a207136eb5fe8c4b541
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2026-06-13 20:40:26 -0300

    fix: stabilize physical analysis extract loading

--- src/main/java/com/migueltcc/fertintelligence/model/fertintelligence/extractAnalysisModels/PhysicalAnalysisExtractModel.java
+++ src/main/java/com/migueltcc/fertintelligence/model/fertintelligence/extractAnalysisModels/PhysicalAnalysisExtractModel.java
@@ -29,150 +29,107 @@ public class PhysicalAnalysisExtractModel {
 
     // Teor de Areia (g/dm3)
     @Column(name = "TEOR_DE_AREIA", nullable = true)
-    double teorAreia;
+    Double teorAreia;
 
     // Teor de Silte (g/dm3)
     @Column(name = "TEOR_DE_SILTE", nullable = true)
-    double teorSilte;
+    Double teorSilte;
 
     // Teor de Argila (g/dm3)
     @Column(name = "TEOR_DE_ARGILA", nullable = true)
-    double teorArgila;
+    Double teorArgila;
 
     // Densidade Aparente (g/cm3)
     @Column(name = "DENSIDADE_APARENTE", nullable = true)
-    double densidadeAparente;
+    Double densidadeAparente;
 
     // Densidade Real (g/cm3)
     @Column(name = "DENSIDADE_REAL", nullable = true)
-    double densidadeReal;
+    Double densidadeReal;
 
     // Porosidade Total (%)
     @Column(name = "POROSIDADE_TOTAL", nullable = true)
-    double porosidadeTotal;
+    Double porosidadeTotal;
 
     // Microporosidade (%)
     @Column(name = "MICROPOROSIDADE", nullable = true)
-    double microporosidade;
+    Double microporosidade;
 
     // Umidade de Capacidade de campo (CC, %)
     @Column(name = "UMIDADE_CAPACIDADE_CAMPO", nullable = true)
-    double umidadeCapacidadeCampo;
+    Double umidadeCapacidadeCampo;
 
     // Umidade de Ponto de Murcha Permanente (PMP, %)
     @Column(name = "UMIDADE_PONTO_MURCHA_PERMANENTE", nullable = true)
-    double umidadePontoMurchaPermanente;
+    Double umidadePontoMurchaPermanente;
 
     // Agua Disponivel (%)
     @Column(name = "AGUA_DISPONIVEL", nullable = true)
-    double aguaDisponivel;
+    Double aguaDisponivel;
 
     // Resistência à penetração (MPa)
     @Column(name = "RESISTENCIA_PENETRACAO", nullable = true)
-    double resistenciaPenetracao;
+    Double resistenciaPenetracao;
 
     // Percentagem de agregados 6,0 mm de diámetro
     @Column(name = "PERC_AGREGADOS_6_0MM", nullable = true)
-    double percAgregados6_0mm;
+    Double percAgregados6_0mm;
 
     // Percentagem de agregados 4,1 a 6.0 mm
     @Column(name = "PERC_AGREGADOS_4_1_A_6_0MM", nullable = true)
-    double percAgregados4_1a6_0mm;
+    Double percAgregados4_1a6_0mm;
 
     // Percentagem de agregados 2,1a 4.0 mm
     @Column(name = "PERC_AGREGADOS_2_1_A_4_0MM", nullable = true)
-    double percAgregados2_1a4_0mm;
+    Double percAgregados2_1a4_0mm;
 
     // Percentagem de 1.0 a 2,0 mm
     @Column(name = "PERC_AGREGADOS_1_0_A_2_0MM", nullable = true)
-    double percAgregados1_0a2_0mm;
+    Double percAgregados1_0a2_0mm;
 
     // Percentagem de agregados 0,5 a 1,0 mm
     @Column(name = "PERC_AGREGADOS_0_5_A_1_0MM", nullable = true)
-    double percAgregados0_5a1_0mm;
+    Double percAgregados0_5a1_0mm;
 
     // Percentagem de agregados 0,25 a 0,5 mm
     @Column(name = "PERC_AGREGADOS_0_25_A_0_5MM", nullable = true)
-    double percAgregados0_25a0_5mm;
+    Double percAgregados0_25a0_5mm;
 
     // Percentagem de agregados < 0,25 mm
     @Column(name = "PERC_AGREGADOS_MENOR_0_25MM", nullable = true)
-    double percAgregadosMenor0_25mm;
+    Double percAgregadosMenor0_25mm;
 
     // Diâmetro médio dos agregados (mm)
     @Column(name = "DM_AGREGADOS", nullable = true)
-    double dmAgregados;
+    Double dmAgregados;
 
     @PrePersist
     @PreUpdate
     public void recalculateComputedFields() {
-        this.porosidadeTotal = densidadeReal != 0.0
-                ? ((densidadeReal - densidadeAparente) / densidadeReal) * 100.0
+        double densidadeRealValue = zeroIfNull(this.densidadeReal);
+        double densidadeAparenteValue = zeroIfNull(this.densidadeAparente);
+
+        this.porosidadeTotal = densidadeRealValue != 0.0
+                ? ((densidadeRealValue - densidadeAparenteValue) / densidadeRealValue) * 100.0
                 : 0.0;
-        this.aguaDisponivel = umidadeCapacidadeCampo - umidadePontoMurchaPermanente;
+
+        this.aguaDisponivel =
+                zeroIfNull(this.umidadeCapacidadeCampo)
+                        - zeroIfNull(this.umidadePontoMurchaPermanente);
 
         this.dmAgregados =
-                ((percAgregados6_0mm * 6.0)
-                        + (percAgregados4_1a6_0mm * 5.05)
-                        + (percAgregados2_1a4_0mm * 3.05)
-                        + (percAgregados1_0a2_0mm * 1.5)
-                        + (percAgregados0_5a1_0mm * 0.75)
-                        + (percAgregados0_25a0_5mm * 0.375)
-                        + (percAgregadosMenor0_25mm * 0.125))
+                ((zeroIfNull(this.percAgregados6_0mm) * 6.0)
+                        + (zeroIfNull(this.percAgregados4_1a6_0mm) * 5.05)
+                        + (zeroIfNull(this.percAgregados2_1a4_0mm) * 3.05)
+                        + (zeroIfNull(this.percAgregados1_0a2_0mm) * 1.5)
+                        + (zeroIfNull(this.percAgregados0_5a1_0mm) * 0.75)
+                        + (zeroIfNull(this.percAgregados0_25a0_5mm) * 0.375)
+                        + (zeroIfNull(this.percAgregadosMenor0_25mm) * 0.125))
                         / 100.0;
     }
 
-    public PhysicalAnalysisExtractResponseDto toDto() {
-        RangeExtractModel range = this.rangeExtract;
-        LayerExtractModel layer = this.layerExtract;
-
-        Long rangeExtractId = null;
-        Long layerExtractId = null;
-        Integer initialDepth = null;
-        Integer finalDepth = null;
-        com.migueltcc.fertintelligence.composedAttributes.soilExtracts.Camada camada = null;
-        Integer subLayer = null;
-
-        if (range != null) {
-            rangeExtractId = range.getId();
-            initialDepth = range.getProfundidade_inicial();
-            finalDepth = range.getProfundidade_final();
-        } else if (layer != null) {
-            layerExtractId = layer.getId();
-            initialDepth = layer.getProfundidade_inicial();
-            finalDepth = layer.getProfundidade_final();
-            camada = layer.getLayer();
-            subLayer = layer.getSub_layer();
-        }
-
-        return PhysicalAnalysisExtractResponseDto.builder()
-                .id(this.id)
-                .rangeExtractId(rangeExtractId)
-                .layerExtractId(layerExtractId)
-                .initialDepth(initialDepth)
-                .finalDepth(finalDepth)
-                .layer(camada)
-                .subLayer(subLayer)
-                .teorAreia(this.teorAreia)
-                .teorSilte(this.teorSilte)
-                .teorArgila(this.teorArgila)
-                .densidadeAparente(this.densidadeAparente)
-                .densidadeReal(this.densidadeReal)
-                .porosidadeTotal(this.porosidadeTotal)
-                .microporosidade(this.microporosidade)
-                .umidadeCapacidadeCampo(this.umidadeCapacidadeCampo)
-                .umidadePontoMurchaPermanente(this.umidadePontoMurchaPermanente)
-                .aguaDisponivel(this.aguaDisponivel)
-                .resistenciaPenetracao(this.resistenciaPenetracao)
-                .percAgregados6_0mm(this.percAgregados6_0mm)
-                .percAgregados4_1a6_0mm(this.percAgregados4_1a6_0mm)
-                .percAgregados2_1a4_0mm(this.percAgregados2_1a4_0mm)
-                .percAgregados1_0a2_0mm(this.percAgregados1_0a2_0mm)
-                .percAgregados0_5a1_0mm(this.percAgregados0_5a1_0mm)
-                .percAgregados0_25a0_5mm(this.percAgregados0_25a0_5mm)
-                .percAgregadosMenor0_25mm(this.percAgregadosMenor0_25mm)
-                .dmAgregados(this.dmAgregados)
-                .build();
+    private double zeroIfNull(Double value) {
+        return value != null ? value : 0.0;
     }
 
 }
